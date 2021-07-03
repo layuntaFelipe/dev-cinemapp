60f05e8238883ebaa2e69d5cf757e60c2c0bbf75
layuntaFelipe <layunta.felipe@gmail.com>
2021-07-03 15:00:24 -0300

    Initial Commit

---
+++ CinemaApp/CinemaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaAppApp.swift
+//  CinemaApp
+//
+//  Created by Felipe Lobo on 03/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CinemaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinemaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CinemaApp
+//
+//  Created by Felipe Lobo on 03/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
